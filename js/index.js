24792d0237e529dcfb4da43a8138bd12a0b58d20
JChan91 <fourbin1221@gmail.com>
2021-08-22 03:42:26 +0900

    서브메뉴

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "검색 😀");
+});
+
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
